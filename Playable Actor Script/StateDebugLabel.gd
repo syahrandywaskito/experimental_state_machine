121597c1b1f01a57a2886c68acdff28899c1a0e3
Syahrandy Waskito <syahrandywaskito666@gmail.com>
2023-11-09 10:20:48 +0700

    Revert "New : Commit for new repo"
    This reverts commit 3ab4ba527677ac08c89bde16f550b98f3d708685.

--- Playable Actor Script/StateDebugLabel.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-@export var state_machine : ActorStateMachine 
-
-func _process(_delta):
-	text = "State : " + state_machine.current_state.name
--- Player Script/StateDebugLabel.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-@export var fsm : FiniteStateMachine
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	text = "State : " + fsm.state.name
